a31e9a3919580d7237cc183aeb26e87269e82f7f
Andrii Tochylov <andretochilov1@gmail.com>
2020-04-25 19:18:27 +0300

    new lab

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,35 @@
 package domain;
-
+/**
+ * Solves the given problem (counts mirrored hours on digital watch)
+ * @author Andrei
+ */
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    
+    private static byte reverse(byte number){
+        byte lastDigit = (byte) (number%10);
+        number = (byte) (number/10);
+        byte revNum = (byte) (lastDigit*10 + number);
+        return revNum;
+    }
+    /**
+     * Finds and counts the number of mirrored hours
+     * @return number of mirrored hours
+     */
+    public static byte Calculate(){
+        byte count = 0;
+        byte hours;
+//        byte minutes; //uncomment this and following if you want mirrored hours to be displayed
+
+        for(byte i = 0; i<24; i++){
+                        hours = i;
+            for(byte j = 0; j<60; j++){
+//                            minutes= j;
+                if(hours == reverse(j)){
+//                System.out.println(hours + ":" + minutes);
+                count++;
+                }
+            }
+        }
+        return count;
     }
 }
--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -6,7 +6,6 @@ public class TestResult {
 
     public static void main(String[] args) {
 
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+        System.out.println("Mirrored hours appear on clock " + Exercise.Calculate() + " times.");
     }
 }
